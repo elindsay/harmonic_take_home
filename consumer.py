005c396a5bdca0783be6949e4fa2689e6a86f17f
Emma Lindsay <emma@decemberdevs.com>
2024-02-20 09:45:16 -0800

    Remove unneeded file

--- consumer.py
+++
@@ -1,8 +0,0 @@
-class Consumer:
-    def __init__(self, name):
-        self.name = name
- 
-    def receive(self, message):
-        print(f"{self.name}"+"received message:"
-              +f"{message}")
-
